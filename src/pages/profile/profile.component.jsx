b0d82586c6454de67786d3326645075aae6a898a
ashleysalinas <ashleysalinas98@protonmail.com>
2022-09-10 21:22:52 -0500

    added user posts to profile

--- src/pages/profile/profile.component.jsx
+++ src/pages/profile/profile.component.jsx
@@ -1,6 +1,7 @@
 import { getMyPosts } from "../../utils/axios";
 import { useEffect, useContext, useState } from 'react';
-import { UserContext } from '../../contexts/user.context'
+import { UserContext } from '../../contexts/user.context';
+
 
 const Profile = () => {
     const { currentUser } = useContext(UserContext);
@@ -9,13 +10,20 @@ const Profile = () => {
         getMyPosts(currentUser)
         .then(res => {
             const { data } = res
-        setMyPosts(data)
-        console.log(myPosts)
+            setMyPosts(data)
         })
     }, [])
     return(
         <div>
             <h1>My Posts</h1>
+            {myPosts.map(({ postTitle, postText, id}) => {
+                return (
+                    <div key={id}>
+                        <h1>{postTitle}</h1>
+                        <h2>{postText}</h2>
+                    </div>
+                )
+            })}
         </div>
     )
 }
